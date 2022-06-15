0d149f9b8330b4b80a044921d71a50ba1183bc3d
Chyanne Robbins <chyannetech@gmail.com>
2022-06-15 03:21:18 -0400

    Frontend Complete

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+
+// Import components
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Route, Switch } from "react-router-dom";
+
+// Import Pages
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+
+function App() {
+  // URL should have heroku URL from backend.
+  const URL = "https://chyannerobbins.herokuapp.com/";
+
+  return (
+    <div className="App">
+      <Header /> 
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/projects">
+          <Projects URL={URL} />
+        </Route>
+        <Route path="/about">
+          <About URL={URL} />
+        </Route>
+        </Switch>
+    </div>
+  );
+}
+
+export default App;
