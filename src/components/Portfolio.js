d96de8bb259b2bc1baf9ce5d0edfda103f28feba
Chyanne Robbins <chyannetech@gmail.com>
2022-08-03 17:19:50 -0400

    Update Projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React from "react";
 
 // import components
-import Projects from './Projects';
+import Projects from "./Projects";
 
 const Portfolio = () => {
   return (
-    <div name="Projects" className="w-full pt-28 h-full bg-[#000000] text-[#c1c0c0]">
-    <section id='projects' className='section bg-[#000000] min-h-[1400px]'>
-      <div className='container pt-28 mx-auto'>
-      <div className=" pl-10 pt-10">
-        <p className="text-4xl font-bold text-white inline center border-b-4 border-[#121212]">
-              Projects
-            </p>
+    <div
+      name="Projects"
+      className="w-full md:h-screen bg-[#000000] text-[#c1c0c0]"
+    >
+      <section
+        id="projects"
+        className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full"
+      >
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-[#121212]">
+            Projects
+          </p>
         </div>
         <Projects />
-      </div>
-      
-    </section>
+      </section>
     </div>
   );
 };
