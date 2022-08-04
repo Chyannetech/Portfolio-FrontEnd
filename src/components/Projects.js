243867ccdcd58ae2d9aa647e70d02c5d362f1338
Chyanne Robbins <chyannetech@gmail.com>
2022-08-03 21:22:46 -0400

    Update Projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -32,6 +32,7 @@ const Projects = () => {
   return (
     <div>
       {/* projects nav */}
+      <h3>My Recent Work</h3>
       <nav className="mb-12 max-w-[1000px] mx-auto py-6">
         <ul className="flex flex-col md:flex-row justify-evenly items-center text-white   pt-3">
           {projectsNav.map((item, index) => {
@@ -51,36 +52,44 @@ const Projects = () => {
           })}
         </ul>
       </nav>
+      
       {/* projects */}
       <section id="projects">
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-          {projectsData.map(({ id, image, title, github, demo, prototype, category }) => {
-            return (
-              <div key={id}>
-                <img src={image} alt={title} />
-                <div>
-                {prototype ? (
-                    <a href={prototype} target="_blank" rel="noreferrer">
-                      <font className="portfolio__demo-text">Prototype</font>
-                    </a>
-                  ) : null}
-                  {github ? (
-                    <a href={github} target="_blank" rel="noreferrer">
-                      <font>Github</font>
-                    </a>
-                  ) : null}
-                  <br></br>
-                  {demo ? (
-                    <a href={demo} target="_blank" rel="noreferrer">
-                      <font className="portfolio__demo-text">Live Demo</font>
-                    </a>
-                  ) : null}
-                
-                  
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-5">
+      
+          {projectsData.map(
+            ({ id, image, title, github, demo, prototype, category }) => {
+              return (
+                <div
+                  className="border-1 mb-10 pl-2 pr-2 pt-2 shadow-md shadow-[#404040] bg-[#0d0d0d]"
+                  key={id}
+                >
+                  <img src={image} alt={title} />
+                  <div>
+                    <p className="text-accent text-md mb-3 text-white">
+                      {category}
+                    </p>
+                    {prototype ? (
+                      <a href={prototype} target="_blank" rel="noreferrer">
+                        <font className="portfolio__demo-text">Prototype</font>
+                      </a>
+                    ) : null}
+                    {github ? (
+                      <a href={github} target="_blank" rel="noreferrer">
+                        <font>Github</font>
+                      </a>
+                    ) : null}
+                    <br></br>
+                    {demo ? (
+                      <a href={demo} target="_blank" rel="noreferrer">
+                        <font className="portfolio__demo-text">Live Demo</font>
+                      </a>
+                    ) : null}
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            }
+          )}
         </div>
       </section>
     </div>
